5a163f7b860ecda364ec0431babdbd3dc3733567
David Choloyan <shamsani.david@gmail.com>
2024-09-28 21:36:00 +0200

    feat: eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/consistent-type-imports": "error",
+  },
+};
